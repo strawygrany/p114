6d7da0faef6bdd106ac9046779eb7685ed877bcc
strawygrany <103514750+strawygrany@users.noreply.github.com>
2023-09-08 18:36:09 +0530

    Add files via upload

---
+++ t1_c114.py
@@ -0,0 +1,8 @@
+a=33
+b=33
+
+if a>b :
+    print(" b is greater than me")
+elif a==b:
+    print("a is equal to b")
+
---
+++ t2_c114.py
@@ -0,0 +1,8 @@
+
+def m_f(fname):
+    fname = "refneses"
+    print (fname)
+    
+m_f("Emil")
+m_f("Tobias")
+m_f("linus")
